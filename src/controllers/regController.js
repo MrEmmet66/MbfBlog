1832b506bfce5274dc374e0c91085386bcbe466a
sxaxq <sxaxq@gmail.com>
2024-02-12 18:23:10 +0300

    push register

---
+++ src/controllers/regController.js
@@ -0,0 +1,33 @@
+const { dirname } = require("path")
+const db = require("../core/db")
+const pathToView = require("../views/views_path")
+
+exports.register_form = function(request, response) {
+    const viewPath = global.date
+    response.sendFile(viewPath + "/regPage.html")
+}
+
+exports.register_get_form = function(request, response) {
+    const username = request.body.username
+    const email = request.body.email
+    const password = request.body.password
+
+    console.log(`Username: ${username}`)
+    console.log(`Email: ${email}`)
+    console.log(`Password: ${password}`)
+
+    db.run(
+        "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+        [username, email, password],
+        function (err) {
+          if (err) {
+            console.error(err);
+            response.status(500).send("Error registering user");
+          } else {
+            console.log("User registered successfully");
+            const viewPath = global.date
+            response.sendFile(viewPath + "/success_register.html")
+          }
+        }
+      )
+}
--- src/core/app.js
+++ src/core/app.js
@@ -1,10 +1,26 @@
 const express = require("express")
 
+const registerController = require("../controllers/regController.js")
+
 const app = express()
 
-app.get("/", function(request, response) {
-    response.send("беброчка")
+const db = require("./db.js")
+
+db.serialize(() => {
+    db.run(
+      "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, email TEXT, password TEXT)"
+    )
 })
+
+app.set("view engine", "hbs")
+app.use(express.urlencoded({extended: false}))
+
+const registerRouter = express.Router()
+
+registerRouter.use("/create_account", registerController.register_form)
+registerRouter.use("/successfully_register", registerController.register_get_form)
+
+app.use("/account", registerRouter)
 
 app.listen(3000, () => {
     console.log("Server stated | http://localhost:3000")
