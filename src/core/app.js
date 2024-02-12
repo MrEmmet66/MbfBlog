cc503dde3bfc35dd93a8a257d35d440113fa005d
sxaxq <sxaxq@gmail.com>
2024-02-12 19:39:44 +0300

    view profile

--- src/core/app.js
+++ src/core/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 
 const registerController = require("../controllers/regController.js")
+const profileViewController = require("../controllers/profileController.js")
 
 const app = express()
 
@@ -16,11 +17,15 @@ app.set("view engine", "hbs")
 app.use(express.urlencoded({extended: false}))
 
 const registerRouter = express.Router()
+const profileViewRouter = express.Router()
 
 registerRouter.use("/create_account", registerController.register_form)
 registerRouter.use("/successfully_register", registerController.register_get_form)
 
+profileViewRouter.use("/:username", profileViewController.check_profile)
+
 app.use("/account", registerRouter)
+app.use("/users", profileViewRouter)
 
 app.listen(3000, () => {
     console.log("Server stated | http://localhost:3000")
